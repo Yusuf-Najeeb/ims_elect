b76b639180158c118d28923d88fe0fed85fd97d9
Muhammad Najeeb <yusnaj21@gmail.com>
2024-10-04 22:03:51 +0100

    Modified: User and Account Models

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use server";
+
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -8,6 +12,10 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields) {
     return { error: "Invalid Credentials" };
   }
+
+  const { name, email, password } = validateFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   return { success: "Email sent" };
 };
