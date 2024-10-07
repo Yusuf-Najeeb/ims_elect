40bac029f7b8c07364c2fe0b0cd5a5b9f3587292
Muhammad Najeeb <yusnaj21@gmail.com>
2024-10-07 19:08:19 +0100

    Modified: Extended the auth session using callbacks

--- actions/login.ts
+++ actions/login.ts
@@ -19,6 +19,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+
+    return { success: "Login Successful" };
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
