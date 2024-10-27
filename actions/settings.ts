0f9b69323e8b645be624c272d31ff3eb6150b43e
Muhammad Najeeb <yusnaj21@gmail.com>
2024-10-27 23:01:32 +0100

    Initialized: settings server action

---
+++ actions/settings.ts
@@ -0,0 +1,15 @@
+"use server";
+import * as z from "zod";
+import { db } from "@/lib/db";
+import { settingsSchema } from "@/schemas";
+import { getUserById } from "@/data/user";
+import { currentUser } from "@/lib/auth";
+
+export const settings = async (values: z.infer<typeof settingsSchema>) => {
+  const user = await currentUser();
+
+  if (!user) return { error: "Unauthorized" };
+
+  const dbUser = await getUserById(user.id);
+  if (!dbUser) return { error: "Unauthorized" };
+};
