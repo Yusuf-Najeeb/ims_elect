eaa7def5f1c0382a025d90d2afb53937b153c724
Muhammad Najeeb <yusnaj21@gmail.com>
2024-10-29 19:43:14 +0100

    initialized: Settings Form

--- actions/settings.ts
+++ actions/settings.ts
@@ -7,10 +7,20 @@ import { currentUser } from "@/lib/auth";
 
 export const settings = async (values: z.infer<typeof settingsSchema>) => {
   const user = await currentUser();
-
   if (!user) return { error: "Unauthorized" };
 
-  const dbUser = await getUserById(user.id);
+  if (user && user.id) {
+    const dbUser = await getUserById(user.id);
 
-  if (!dbUser) return { error: "Unauthorized" };
+    if (!dbUser) return { error: "Unauthorized" };
+
+    await db.user.update({
+      where: { id: dbUser.id },
+      data: {
+        ...values,
+      },
+    });
+
+    return { success: "Settings Updated" };
+  }
 };
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,37 +1,66 @@
 "use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { settingsSchema } from "@/schemas";
 
-import { signOut } from "next-auth/react";
+import { useTransition, useState } from "react";
+import { useSession } from "next-auth/react";
+import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
-
-// import { useCurrentUser } from "@/hooks/use-current-user";
-
-// import { auth, signOut } from "@/auth";
-
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormLabel,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormDescription,
+} from "@/components/ui/form";
+import { input } from "@/components/ui/input";
 const SettingsPage = () => {
-  // const session = await auth();
-  // const session = useSession();
-  // const user = useCurrentUser();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
 
-  const logOut = () => signOut();
+  const [isPending, startTransition] = useTransition();
+  const { update } = useSession();
 
+  const form = useForm<z.infer<typeof settingsSchema>>({
+    resolver: zodResolver(settingsSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof settingsSchema>) => {
+    startTransition(() => {
+      settings(values)
+        .then((data) => {
+          if (data?.error) {
+            setError(data.error);
+            setSuccess("");
+          }
+
+          if (data?.success) {
+            setSuccess(data.success);
+            setError("");
+          }
+        })
+        .catch(() => setError("Something went wrong"));
+    });
+  };
   return (
-    <div className="bg-white p-10 rounded-xl">
-      <Button type="submit" variant="secondary" size="sm" onClick={logOut}>
-        Sign out
-      </Button>
+    <Card className="w-[600px] shadow-sm">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">⚙ Settings</p>
+      </CardHeader>
 
-      {/* <form
-      action={async () => {
-        "use server";
-
-        await signOut();
-      }}
-      >
-        <Button type="submit" variant="secondary" size="sm">
-          Sign out
-        </Button>
-      </form> */}
-    </div>
+      <CardContent>
+        <Form {...form}>
+          <form className="flex flex-col gap-4"></form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
